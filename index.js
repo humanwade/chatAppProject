f64e80d26cdc3299874aca68ee7e5d71bb84d7ec
WadeJung <seounghoon.wade@gmail.com>
2025-10-31 00:24:08 -0400

    Sync local changes after lost .git

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ require("dotenv").config();
 const initRooms = require("./Utils/initRooms");
 require("./Models/room");
 
-
 const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
@@ -20,5 +19,5 @@ initRooms();
 require("./Utils/io")(io);
 
 httpServer.listen(process.env.PORT, () => {
-    console.log("server listening on port", process.env.PORT);
+  console.log("server listening on port", process.env.PORT);
 });
